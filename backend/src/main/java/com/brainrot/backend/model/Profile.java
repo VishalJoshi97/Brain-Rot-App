887f43652d3adce928fd3f7db3ab2868ccd5d3ab
Vishal Joshi <dharmendrakulkarni962@gmail.com>
2026-02-20 18:34:24 +0530

    add profile columns.

--- backend/src/main/java/com/brainrot/backend/model/Profile.java
+++ backend/src/main/java/com/brainrot/backend/model/Profile.java
@@ -1,4 +1,26 @@
 package com.brainrot.backend.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String bio;
+
+    private String profilePictureUrl;
+
+    private int totalCore;
+
+    @OneToOne
+    @JoinColumn(name = "user_id",nullable = false,unique = true)
+    private User user;
 }
