a9ca85e1e34fb6704a1efe210a0131e67d8b82b0
Vishal Joshi <dharmendrakulkarni962@gmail.com>
2026-02-20 18:18:05 +0530

    add user columns.

--- backend/src/main/java/com/brainrot/backend/model/User.java
+++ backend/src/main/java/com/brainrot/backend/model/User.java
@@ -1,14 +1,28 @@
 package com.brainrot.backend.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.*;
+import org.springframework.data.annotation.Id;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false,unique = true)
     private String name;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false,unique = true)
     private String email;
 }
