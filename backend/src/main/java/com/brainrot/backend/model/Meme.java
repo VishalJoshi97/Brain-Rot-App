92f43d0261015465b9e4b231fec7358b79d8176c
Vishal Joshi <dharmendrakulkarni962@gmail.com>
2026-02-20 18:27:10 +0530

    add meme columns.

--- backend/src/main/java/com/brainrot/backend/model/Meme.java
+++ backend/src/main/java/com/brainrot/backend/model/Meme.java
@@ -1,4 +1,27 @@
 package com.brainrot.backend.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Meme {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String imageUrl;
+
+    private int likes;
+
+    @ManyToMany//many memes to one user
+    @JoinColumn(name="user_id")
+    private User user;
+
+
 }
