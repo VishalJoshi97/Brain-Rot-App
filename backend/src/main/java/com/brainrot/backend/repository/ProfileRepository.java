b4674142b64741877baf21245b7e158f2e0f7cc4
Vishal Joshi <dharmendrakulkarni962@gmail.com>
2026-02-20 18:51:42 +0530

    add profile repo.

---
+++ backend/src/main/java/com/brainrot/backend/repository/ProfileRepository.java
@@ -0,0 +1,10 @@
+package com.brainrot.backend.repository;
+
+import com.brainrot.backend.model.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProfileRepository extends JpaRepository<Profile,Long> {
+     Optional<Profile> findByUserId(Long userId);
+}
