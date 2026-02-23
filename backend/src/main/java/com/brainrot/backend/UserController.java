41873bcc4a045ec21e98e0046c57690cb5f28bfa
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2026-02-23 19:36:37 +0530

    Create UserController for user management API

---
+++ backend/src/main/java/com/brainrot/backend/UserController.java
@@ -0,0 +1,15 @@
+package com.brainrot.controller;
+
+import com.brainrot.dto.UserDTO;
+import com.brainrot.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+}
