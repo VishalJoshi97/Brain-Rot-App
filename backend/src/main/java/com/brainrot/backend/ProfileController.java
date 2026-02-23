517036ab6029787268ecc0179954316baf1e8c69
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2026-02-23 19:43:06 +0530

    Create ProfileController for profile management

---
+++ backend/src/main/java/com/brainrot/backend/ProfileController.java
@@ -0,0 +1,13 @@
+package com.brainrot.controller;
+
+import com.brainrot.dto.ProfileDTO;
+import com.brainrot.service.ProfileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/profile")
+@RequiredArgsConstructor
+public class ProfileController {
+  
+}
