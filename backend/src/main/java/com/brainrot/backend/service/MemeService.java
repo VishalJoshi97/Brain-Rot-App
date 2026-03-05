bad5d19848f3889fc5d35af196e743a5b0de5a1e
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2026-03-05 23:46:29 +0530

    Add MemeService to service

---
+++ backend/src/main/java/com/brainrot/backend/service/MemeService.java
@@ -0,0 +1,15 @@
+package com.brainrot.backend.modules.meme.service;
+
+import com.brainrot.modules.meme.dto.MemeDTO;
+
+import java.util.List;
+
+public interface MemeService {
+
+    MemeDTO uploadMeme(MemeDTO memeDTO);
+
+    List<MemeDTO> getAllMemes();
+
+    MemeDTO likeMeme(Long memeId);
+
+}
