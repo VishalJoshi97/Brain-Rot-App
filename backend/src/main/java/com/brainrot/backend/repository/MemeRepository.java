be9812ed2b73c1961eafa85108c83e8cd8164dbf
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2026-03-07 22:38:47 +0530

    Add pagination to findAllByOrderByCreatedAtDesc

--- backend/src/main/java/com/brainrot/backend/repository/MemeRepository.java
+++ backend/src/main/java/com/brainrot/backend/repository/MemeRepository.java
@@ -2,10 +2,13 @@ package com.brainrot.backend.repository;
 
 import com.brainrot.backend.model.Meme;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface MemeRepository extends JpaRepository<Meme,Long> {
 
     List<Meme> findAllByOrderByLikesDesc();
+    Page<Meme> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
