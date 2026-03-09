da91502282ce0758560a3db1a2aaca69eecb48eb
VishalJoshi97 <dharmendrakulkarni962@gmail.com>
2026-03-09 23:35:28 +0530

    Create CommentDTO.java

---
+++ backend/src/main/java/com/brainrot/backend/dto/CommentDTO.java
@@ -0,0 +1,15 @@
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentDTO {
+
+    private Long id;
+
+    private Long memeId;
+
+    private Long userId;
+
+    private String text;
+
+}
