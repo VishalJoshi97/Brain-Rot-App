28756932d94b0f7d2f07a0dee7a5f06f89be66ef
Vishal Joshi <dharmendrakulkarni962@gmail.com>
2026-03-13 16:06:43 +0530

    implement user service.

--- backend/src/main/java/com/brainrot/backend/service/UserServiceImpl.java
+++ backend/src/main/java/com/brainrot/backend/service/UserServiceImpl.java
@@ -24,6 +24,7 @@ public class UserServiceImpl implements UserService {
                 .build();
 
         user = userRepository.save(user);
+        //instead of getter
         return new UserDTO(user.getId(), user.getEmail(), user.getName() );
     }
 
