fe516ef3f1f3723cd26612bd8d4e50c8947bb977
Vishal Joshi <dharmendrakulkarni962@gmail.com>
2026-03-13 16:04:57 +0530

    update brain repos.

--- backend/src/main/java/com/brainrot/backend/repository/BrainRotActivityRepository.java
+++ backend/src/main/java/com/brainrot/backend/repository/BrainRotActivityRepository.java
@@ -4,4 +4,5 @@ import com.brainrot.backend.model.BrainRotActivity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BrainRotActivityRepository extends JpaRepository<BrainRotActivity,Long> {
+    String findActivityType(Long userId);
 }
--- backend/src/main/java/com/brainrot/backend/repository/BrainRotTestRepository.java
+++ backend/src/main/java/com/brainrot/backend/repository/BrainRotTestRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BrainRotTestRepository extends JpaRepository<BrainRotTestRepository,Long> {
-    List<BrainRotTestRepository> findTestByuserId(Long userId);
+    List<BrainRotTestRepository> findTestByUserId(Long userId);
 }
