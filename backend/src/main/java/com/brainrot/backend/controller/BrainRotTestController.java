7b1788658c67e1a37adc67e89ada8b15135d670c
Vishal Joshi <dharmendrakulkarni962@gmail.com>
2026-03-13 16:02:33 +0530

    update test controller

--- backend/src/main/java/com/brainrot/backend/controller/BrainRotTestController.java
+++ backend/src/main/java/com/brainrot/backend/controller/BrainRotTestController.java
@@ -1,3 +1,14 @@
+package com.brainrot.backend.controller;
+
+import com.brainrot.backend.dto.BrainRotTestRequestDTO;
+import com.brainrot.backend.dto.BrainRotTestResultDTO;
+import com.brainrot.backend.service.BrainRotTestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api/brainrot-test")
 @RequiredArgsConstructor
