c2c91f85f81da24118f330243dd1cf5c3011b285
Vishal Joshi <dharmendrakulkarni962@gmail.com>
2026-03-13 16:07:01 +0530

    update layout

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,19 +1,13 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
- 
-
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
+ 
   return (
-    <ThemeProvider value={colorScheme === 'white' ? DarkTheme : DefaultTheme}>
+    
       <Stack screenOptions={{headerShown:false}}>
          <Stack.Screen name="index" />
       </Stack>
-     </ThemeProvider>
+     
   );
 }
